5f0c5731f601191fb6aaafde81925e0ed0153f91
aymangamal74 <81592762+aymangamal74@users.noreply.github.com>
2025-12-05 00:59:46 +0200

    Add files via upload

--- 2-Designing/atl/schema.sql
+++ 2-Designing/atl/schema.sql
@@ -1,1 +1,30 @@
+CREATE TABLE "passengers"(
+    "fname" TEXT NOT NULL ,
+    "lname" TEXT NOT NULL,
+    "age" NUMERIC NOT NULL,
+);
+CREATE TABLE "airlines"(
+    "id" INTEGER,
+    PRIMARY KEY("id"),
+    "name" TEXT NOT NULL,
+    "section" TEXT NOT NULL 
+);
 
+CREATE TABLE "check_ins"(
+    "date_time" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    "flight_id" INTEGER ,
+    Foreign Key ("flight_id") REFERENCES "flights" ("id")
+
+);
+
+CREATE TABLE "flights"(
+    "id" INTEGER,
+    "flight_number" INTEGER NOT NULL ,
+    "departing_airport" TEXT NOT NULL,
+    "arriving_airport" TEXT NOT NULL,
+    "deptime" NUMERIC NOT NULL,
+    "arvtime" NUMERIC NOT NULL ,
+    "date" NUMERIC,
+    Foreign Key ("id") REFERENCES "airlines"("id")
+
+);
